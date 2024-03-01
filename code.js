cc18b807f98e7669aab0db8a3576fc80280b3395
Keifer <144736945+kbuss26@users.noreply.github.com>
2024-03-01 11:52:27 -0700

    Solution #1 code.js
    Pretty sure this won't work since I assumed the vertices did not start at 0

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
+/*
+    Keifer Buss 
+    COSC-3020-01 
+    Last modified Mar. 01 2024
+    Sources: None for this program
+*/
+
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    // Variable declarations
+    var adjList = [];
+    var edgeList = [];
+    var edgeExists = 0;
+
+    // Pseudo-nested for loop - we can assume the bound is |V|^2 since the both dimensions = |V|.
+    for (var i = 0; i < (adjMatrix.length) ** 2; i++) {
+        // Get value for position
+        edgeExists = adjMatrix[Math.floor(i / adjMatrix.length)][i % adjMatrix.length];
+        if (edgeExists) edgeList.push((i % adjMatrix.length) + 1); // Stores edge position in list if non-zero
+        if ((i + 1) % adjMatrix.length === 0) {
+            // Resets if reaches end of first matrix row
+            adjList.push(edgeList);
+            edgeList = [];
+        }
+    }
+    
+    return adjList;
 }
